fb0aefecfb09a1cd99a8766e761f8d6a992bf369
Martin J. Di Liscia <mjdiliscia@gmail.com>
2025-05-22 20:31:44 +0200

    Cleaned up the whole code.

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// The main script for the extension
-
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 import { SlashCommandParser } from '../../../slash-commands/SlashCommandParser.js';
 import { SlashCommandArgument, ARGUMENT_TYPE } from '../../../slash-commands/SlashCommandArgument.js';
 import { SlashCommand } from '../../../slash-commands/SlashCommand.js';
-
-//You'll likely need to import some other functions from the main script
 import { saveSettingsDebounced, event_types, eventSource, sendMessageAsUser } from "../../../../script.js";
 
-// Keep track of where your extension is located, name should match repo name
 const oocImagePath = "img/ooc.png";
+const oocToken = "((Out-of-Character))";
+const oocCharacterName = "Out of Character";
 const extensionName = "SillyTavern-Lucid";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
@@ -19,108 +15,176 @@ const defaultSettings = {
   assistantTag: "lucid_assistant"
 };
 
+/**
+ * Finds and replaces all narrator, assistant and ooc headers with those expected by Lucid, in all historic messages.
+ * This only supports text completion APIs.
+ *
+ * @param {object} ST pre-generation data object.
+ */
 function onPromptGenerated(data) {
   const context = getContext();
 
+  // Resolve narrator and assistant tags
   const lucidNarratorTag = context.tags.find((value) => { return value.name == extensionSettings.narratorTag; });
   const lucidNarratorTagId = lucidNarratorTag ? lucidNarratorTag.id : null;
   const lucidAssistantTag = context.tags.find((value) => { return value.name == extensionSettings.assistantTag; });
   const lucidAssistantTagId = lucidAssistantTag ? lucidAssistantTag.id : null;
 
-  let messages = data.finalMesSend;
-  let oocRE = new RegExp(`<\\|start_header_id\\|>writer character ([^<]+)<\\|end_header_id\\|>([\\n\\r]*)\\(\\(Out-of-Character\\)\\)`);
+  // Prepare regular expressions
+  const escapedOocToken = oocToken.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  let oocRE = new RegExp(`<\\|start_header_id\\|>writer character ([^<]+)<\\|end_header_id\\|>([\\n\\r]*)${escapedOocToken}`);
   let generalRE = new RegExp(`<\\|start_header_id\\|>writer character ([^<]+)<\\|end_header_id\\|>`, "g");
 
+  // Iterate messages and replace headers as needed
+  let messages = data.finalMesSend;
   for (let index = 0; index < messages.length; index++) {
     let currentMessage = messages[index];
 
-    if (lucidNarratorTagId || lucidAssistantTagId) {
-      let match = generalRE.exec(currentMessage.message);
-      while (match) {
-        const characterName = match[1];
-        const character = context.characters.find((value) => { return value.name == characterName; });
-        if (!character) {
-          match = generalRE.exec(currentMessage.message);
-          continue;
-        }
+    tryToReplaceSpecialCharacters(currentMessage, lucidNarratorTagId, lucidAssistantTagId, generalRE, context);
+    tryToReplaceOOC(currentMessage, oocRE);
+  }
+}
 
-        const tags = context.tagMap[character.avatar];
+/**
+ * Replaces all found narrator and assistant headers with those expected by Lucid.
+ *
+ * @param {object} messageBox An object containing a message field with the raw message string. (In/Out parameter)
+ * @param {string} narratorTag Character tag that identifies a character as a narrator.
+ * @param {string} assistantTag Character tag that identifies a character as a assistant.
+ * @param {RegExp} regex Regular expression which matches all character headers.
+ * @param {object} ST context object.
+ */
+function tryToReplaceSpecialCharacters(messageBox, narratorTag, assistantTag, regex, context) {
+  if (narratorTag || assistantTag) {
+    // Find message start
+    let match = regex.exec(messageBox.message);
 
-        if (tags.includes(lucidNarratorTagId)) {
-          currentMessage.message = currentMessage.message.replace(match[0], "<|start_header_id|>writer narrative<|end_header_id|>")
-        } else if (tags.includes(lucidAssistantTagId)) {
-          currentMessage.message = currentMessage.message.replace(match[0], "<|start_header_id|>assistant<|end_header_id|>")
-        }
-
-        match = generalRE.exec(currentMessage.message);
-      }
-    }
-
-    let match = currentMessage.message.match(oocRE);
     while (match) {
-      currentMessage.message = currentMessage.message.replace(oocRE, "<|start_header_id|>user<|end_header_id|>$2")
-      match = currentMessage.message.match(oocRE);
+      // Find which character is owner of the message
+      const characterName = match[1];
+      const character = context.characters.find((value) => { return value.name == characterName; });
+      if (!character) {
+        // Couldn't find a valid character as owner. Skipping. This shouldn't happen.
+        match = regex.exec(messageBox.message);
+        continue;
+      }
+
+      // Get owner character tags
+      const tags = context.tagMap[character.avatar];
+
+      // Replace header according to character's tags
+      if (tags.includes(narratorTag)) {
+        messageBox.message = messageBox.message.replace(match[0], "<|start_header_id|>writer narrative<|end_header_id|>")
+      } else if (tags.includes(assistantTag)) {
+        messageBox.message = messageBox.message.replace(match[0], "<|start_header_id|>assistant<|end_header_id|>")
+      }
+
+      // Find next message start
+      match = regex.exec(messageBox.message);
     }
   }
 }
 
-function onChatUpdated(messageId) {
-  let context = getContext();
+/**
+ * Replaces all found ooc headers with the one expected by Lucid.
+ *
+ * @param {object} messageBox An object containing a message field with the raw message string. (In/Out parameter)
+ * @param {RegExp} regex The regular expression which matches every OOC header+token.
+ */
+function tryToReplaceOOC(messageBox, regex) {
+  // Find message start
+  let match = messageBox.message.match(regex);
+
+  while (match) {
+    // Replace ooc header and token
+    messageBox.message = messageBox.message.replace(regex, "<|start_header_id|>user<|end_header_id|>$2")
+
+    // Find next message start
+    match = messageBox.message.match(regex);
+  }
+}
+
+/**
+ * Updates the chat UI replacing raw ooc messages with the expected appearance.
+ * Only alters user messages prefixed with the OOC token.
+ */
+function onChatUpdated(_) {
+  const context = getContext();
+
+  // Iterate through all chat messages
   let chat = $("#chat .mes");
-
   chat.each((index, mes) => {
-    let messageId = Number($(mes).attr("mesid"));
-    let chatSpec = context.chat[messageId];
+    const messageId = Number($(mes).attr("mesid"));
+    const chatSpec = context.chat[messageId];
 
-    if (chatSpec.is_user && !chatSpec.is_system && chatSpec.mes.startsWith("((Out-of-Character))")) {
+    // Only process user, non-system, messages starting with the OOC token
+    if (chatSpec.is_user && !chatSpec.is_system && chatSpec.mes.startsWith(oocToken)) {
       // Change is_user attribute for styling purposes
       $(mes).attr("is_user", "false");
 
       // Remove ((Out-of-Character)) mark
       let message = $(mes).find(".mes_text").children("p").first();
       let messageHTML = message.html();
-      messageHTML = messageHTML.replace("((Out-of-Character))<br>", "");
+      messageHTML = messageHTML.replace(`${oocToken}<br>`, "");
       message.html(messageHTML);
 
       // Change character name to "Out of Character"
-      $(mes).find(".name_text").text("Out of Character");
+      $(mes).find(".name_text").text(oocCharacterName);
 
       // Change avatar to ooc image
       let avatarImg = $(mes).find(".avatar img");
-      let occPath = "/" + extensionFolderPath + "/" + oocImagePath;
+      const occPath = "/" + extensionFolderPath + "/" + oocImagePath;
       avatarImg.attr("src", occPath);
     }
   });
 }
 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
+/**
+ * Loads the stored extension settings and initializes unset values.
+ */
 async function loadSettings() {
+  // Initialize unset settings with their default values
   for (let property in defaultSettings) {
     if (extensionSettings[property] === undefined) {
       extensionSettings[property] = defaultSettings[property];
     }
   }
 
+  // Set current settings values to the settings UI.
   $("#narrator_tag_setting").prop("value", extensionSettings.narratorTag);
   $("#assistant_tag_setting").prop("value", extensionSettings.assistantTag);
 }
 
+/**
+ * Updates the narrator tag setting.
+ */
 function onNarratorTagChanged(event) {
   extensionSettings.narratorTag = $(event.target).prop("value");
   saveSettingsDebounced();
 }
 
+/**
+ * Updates the assistant tag setting.
+ */
 function onAssistantTagChanged(event) {
   extensionSettings.assistantTag = $(event.target).prop("value");
   saveSettingsDebounced();
 }
 
-function runOOCSlashCommand(namedArgs, unnamedArgs) {
-  let message = "((Out-of-Character))\n" + unnamedArgs.toString();
+/**
+ * Actual OOC slash command execution.
+ * Just prepends the OOC token before the user message.
+ */
+function runOOCSlashCommand(_, unnamedArgs) {
+  const message = `${oocToken}\n` + unnamedArgs.toString();
   sendMessageAsUser(message);
   return message;
 }
 
+/**
+ * Sets up the OOC slash command.
+ * This is just ST slash command configuration.
+ */
 function setupOOCSlashCommand() {
   SlashCommandParser.addCommandObject(SlashCommand.fromProps({
     name: 'out-of-character',
@@ -156,24 +220,28 @@ function setupOOCSlashCommand() {
   }));
 }
 
-// This function is called when the extension is loaded
+/**
+ * Extension initialization
+ */
 jQuery(async () => {
-  // This is an example of loading HTML from a file
+  // Get our settings UI node
   const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
 
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
+  // Paste our settings UI
   $("#extensions_settings").append(settingsHtml);
 
-  // Load settings when starting things up (if you have any)
+  // Handle settings loading and saving
   loadSettings();
   $("#narrator_tag_setting").on("input", onNarratorTagChanged);
   $("#assistant_tag_setting").on("input", onAssistantTagChanged);
 
+  // Setup ooc slash command
   setupOOCSlashCommand();
 
+  // Set hook to alter prompt headers just before LLM generation request
   eventSource.on(event_types.GENERATE_BEFORE_COMBINE_PROMPTS, onPromptGenerated);
+
+  // Set hooks to alter the user chat UI by replacing and formatting OOC messages correctly
   eventSource.on(event_types.MORE_MESSAGES_LOADED, onChatUpdated);
   eventSource.on(event_types.CHAT_CHANGED, onChatUpdated);
   eventSource.on(event_types.MESSAGE_SWIPED, onChatUpdated);
